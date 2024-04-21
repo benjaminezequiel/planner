f35b2ef82482a099c5a4017fbf1e044a9f1eb255
ezben <ben.ezequiel08@gmail.com>
2024-04-21 01:42:10 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Input from './components/input.vue'
+
+</script>
+
+<template>
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main> -->
+  <Input />
+  <Input />
+  <Input />
+</template>
+
+<script lang="ts">
+
+const countModel = "24";
+console.log(Input.message)
+
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/input.vue
@@ -0,0 +1,25 @@
+
+
+<script lang="ts">
+
+import { defineComponent, defineProps } from 'vue';
+
+export default defineComponent({
+    component: 'Input',
+});
+
+</script>
+
+<template>
+    <div class="container">
+        <label>Label Text</label>
+        <input>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+}
+</style>
